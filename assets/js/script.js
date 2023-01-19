8497998d6dd238a84c6c777f9e54ffa2ccea69a7
MaccersM8 <117213310+MaccersM8@users.noreply.github.com>
2023-01-19 13:03:11 +0000

    Calculated And Displayed Current Date And Time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,32 @@
 // PSUEDOCODE FOR PROGRAM
 
 // Create Variable For Time Of Given Day
-        // Current Hour (   Moment.js = moment().format('h')   )
-        // Current AM/PM (    Moment.js = moment().format('a')   )
-        // Current Time = Current Hour + Current AM/PM
+// Current Hour (   Moment.js = moment().format('h')   )
+var CurrentHour = moment().format('h');
+console.log("The Current Hour Is " + CurrentHour);
+// Current AM/PM (    Moment.js = moment().format('a')   )
+var CurrentAMPM = moment().format('a');
+console.log("The Time Is Currently In The " + CurrentAMPM);
+// Current Time = Current Hour + Current AM/PM
+var CurrentTime = moment().format('ha');
+console.log("The Current Time To The Nearest Hour Is " + CurrentTime);
 
 // Create Variable For Date Of Given Day
-        // Current Day (   Moment.js = "moment().format('dddd');  )
-        // Current Date (   Moment.js = "moment().format('MMMM Do YYYY');   )
-
+// Current Day (   Moment.js = "moment().format('dddd');  )
+var CurrentDay = moment().format('dddd');
+console.log("Today Is " + CurrentDay);
+// Current Date (   Moment.js = "moment().format('MMMM Do YYYY');   )
+var CurrentDate = moment().format('Do MMMM YYYY');
+console.log("The Current Date Is " + CurrentDate);
 // Create Variable For Full Date
-        // Full Date = Current Day + Current Date
-        // Display Full Date On Web Page
+// Full Date = Current Day + Current Date
+var FullDate = moment().format('dddd Do MMMM YYYY');
+console.log("The Full Date Is " + FullDate);
+// Display Full Date On Web Page
+$("#currentDay").text(FullDate);
+
+var FullTime = moment().format('h:mm a');
+$('#currentTime').text("Time: " + FullTime);
 
 // Function For 9AM
         // IF Current Time < 9AM
